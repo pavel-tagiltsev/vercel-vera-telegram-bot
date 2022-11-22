aac17c91d81f0e299e15a5c733267dd44cdd92af
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2022-11-22 20:45:07 +0700

    Adding a test function

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import notifyBySchedule from "./telegram/functions/notifyBySchedule.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -16,6 +16,8 @@ app.listen(PORT, (error) => {
 
   console.log(`Server is running on port: ${PORT}`);
 });
+
+setInterval(() => console.log("Running"), 5000);
 
 bot.launch();
 notifyBySchedule(process.env.TIME_TO_NOTIFY.split(","));
