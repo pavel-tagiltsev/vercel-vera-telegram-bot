1c23dfc06af40c8004152fdeb4e6a109b8c6b74e
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2022-11-25 02:38:12 +0700

    Changing tha path

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,5 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 export default (app) => {
   app.get("/", (_, res) => {
-    res.sendFile(path.join(__dirname, "/public", "/index.html"));
+    res.sendFile("../public/index.html");
   });
 };
