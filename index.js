df42d87e07026ad0acc8a29c67981fad071b7c8f
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2022-11-25 02:23:13 +0700

    Adding a static page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import bot from "./telegram/bot.js";
 import hooks from "./telegram/hooks/index.js";
+import routes from "./routes/index.js";
 
 dotenv.config();
 
@@ -10,8 +11,10 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
+app.use(express.static("public"));
 app.use(await bot.createWebhook({ domain: process.env.WEBHOOK_DOMAIN }));
 
+routes(app);
 hooks.notify(app);
 
 app.listen(PORT, (error) => {
