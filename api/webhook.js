7f5a658c66ce7c46d5d012262f4c101d74953cf4
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2022-11-25 23:35:06 +0700

    Ridding of async function

--- api/webhook.js
+++ api/webhook.js
@@ -1,14 +1,14 @@
 import bot from "../bot/index.js";
 import commands from "../bot/commands/index.js";
 
-export default async function webhook(req, res) {
+export default function webhook(req, res) {
   if (req.body.notify === process.env.NOTIFY_SECRET) {
     commands.notify();
     return res.send("OK");
   }
 
   try {
-    await bot.handleUpdate(req.body, res);
+    bot.handleUpdate(req.body, res);
   } catch (err) {
     console.error("HANDLE_UPDATE", err);
   }
