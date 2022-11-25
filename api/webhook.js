b2155f5f712a265b77ffa846424f59d0539b0db9
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2022-11-25 19:06:52 +0700

    Transporting to vercel

--- api/webhook.js
+++ api/webhook.js
@@ -1,9 +1,6 @@
 import bot from "../telegram/bot.js";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-bot.telegram.setWebhook(process.env.WEBHOOK_DOMAIN);
+bot.telegram.setWebhook(process.env.VERCEL_URL);
 
 export default function webhook(req, res) {
   bot.handleUpdate(req.body, res);
