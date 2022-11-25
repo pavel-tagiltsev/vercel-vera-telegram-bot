fb695dd0f8f7156406841cd9e2498073c4e6b6d7
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2022-11-25 23:57:11 +0700

    Adding try and catch

--- api/webhook.js
+++ api/webhook.js
@@ -2,14 +2,14 @@ import bot from "../bot/index.js";
 import commands from "../bot/commands/index.js";
 
 export default async function webhook(req, res) {
-  if (req.body.notify === process.env.NOTIFY_SECRET) {
-    await commands.notify(bot);
-    return res.send("OK");
-  }
-
   try {
+    if (req.body.notify === process.env.NOTIFY_SECRET) {
+      await commands.notify(bot);
+      return res.send("OK");
+    }
+
     await bot.handleUpdate(req.body, res);
   } catch (err) {
-    console.error("HANDLE_UPDATE", err);
+    console.error("WEBHOOK", err);
   }
 }
